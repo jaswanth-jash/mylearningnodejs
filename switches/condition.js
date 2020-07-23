f111d08a807fcac4a3bec5e593ba2311ae7fdc72
jaswanth-jash <jaswanthg12@gmail.com>
2020-07-23 08:42:02 +0530

    for loops

---
+++ switches/condition.js
@@ -0,0 +1,10 @@
+// which out switch statements
+var num1 = 30;
+var num2 = 30;
+if (num1 > num2) {
+    console.log('num1 is greater than num2');
+} else if (num1 < num2) {
+    console.log('num1 is less than num2');
+} else {
+    console.log('num1 is equal to num2');
+}
